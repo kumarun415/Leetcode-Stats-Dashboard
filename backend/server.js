f0193b82b37a1517cef03f79af9e593005999e4d
kumarun415 <kumarun415@gmail.com>
2026-09-05 02:12:19 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -121,7 +121,7 @@ app.get("/api/leetcode/:username", async (req, res) => {
 // Start Server
 // ==============================
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
